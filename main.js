adad829fc2c06a5af36d82524add72771f4115f6
Eyder ACM <eyderacm@gmail.com>
2018-11-28 22:01:44 -0600

    add text test

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector(`.words`);
+words.appendChild(p);
+
+recognition.addEventListener('results', e => {
+    console.log(e);
+})
